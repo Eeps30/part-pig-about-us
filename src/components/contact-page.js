8bee4268a997fcef85e4cfd47d81e57d6afc69ed
Eeps30 <eeps30@gmail.com>
2018-04-15 21:09:27 -0700

    started page

--- src/components/contact-page.js
+++ src/components/contact-page.js
@@ -4,20 +4,42 @@ class ContactPage extends Component {
     
     render(){
 
-        const info = {
-            person1 = {
-                name: David Forman,
-                github: 
+        const info = [
+            user1 = {
+                name: 'David Forman',
+                role: 'Project Owner, Front End Development',
+                github: 'https://github.com/daveforman',
+                linkedIn: 'placeholderText'
+            },
+            user2 = {
+                name: 'Alejandro Gaspar',
+                role: 'Front End Development',
+                github: 'https://github.com/caxpper',
+                linkedIn: 'placeholderText'
+            },
+            user3 = {
+                name: 'Evan Ippolito',
+                role: 'Front End Development',
+                github: 'https://github.com/Eeps30',
+                linkedIn: 'placeholderText'
+            },
+            user4 = {
+                name: 'Li Zhang',
+                role: 'Server Side Development',
+                github: 'https://github.com/ByLiZhang',
+                linkedIn: 'placeholderText'
+            },
+            user5 = {
+                name: 'Brian Morris',
+                role: 'Server Side Development',
+                github: 'https://github.com/Briantmorr',
+                linkedIn: 'placeholderText'
             }
-        }
+        ]
 
         return(
             <div className="container">
-                <div className=""></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+              <h1>Replace Me!</h1>
             </div>
         )
     }
